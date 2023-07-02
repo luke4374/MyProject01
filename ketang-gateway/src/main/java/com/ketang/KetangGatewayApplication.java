3040817cf9ebf9ba2924b0b0a464debb67de766c
Laptop_luke <luke31312@sina.com>
2023-07-02 16:31:01 +0800

    添加nacos服务注册与gateway网关

---
+++ ketang-gateway/src/main/java/com/ketang/KetangGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ketang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KetangGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KetangGatewayApplication.class, args);
+    }
+
+}
